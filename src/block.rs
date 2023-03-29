8640482f8f17bec56af2b3dde9c743d1bba36fed
rabuu <rabuu.dev@gmail.com>
2023-03-29 17:36:42 +0200

    Implement `shift_rows`

--- src/block.rs
+++ src/block.rs
@@ -17,4 +17,22 @@ impl Block {
             }
         }
     }
+
+    pub fn shift_rows(&mut self) {
+        for (i, row) in self.bytes.iter_mut().enumerate() {
+            shift_row(row, i as isize);
+        }
+    }
+}
+
+fn shift_row(row: &mut [u8; 4], shift: isize) {
+    if shift == 0 {
+        return;
+    }
+
+    let copy = *row;
+    for (i, byte) in row.iter_mut().enumerate() {
+        let shift_idx = ((i as isize - shift) % 4) as usize;
+        *byte = copy[shift_idx];
+    }
 }
