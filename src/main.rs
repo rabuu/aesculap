04c9eb5ba46dcb9df09b7ead5dca71c2ee26b223
rabuu <rabuu.dev@gmail.com>
2023-04-29 00:29:13 +0200

    Write a (failing!) test

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,27 @@
-use aesculap::key::AES128Key;
+use aesculap::{block::Block, key::AES128Key};
 
 fn main() {
-    println!("{:?}", aesculap::util::rot_left([1, 2, 3, 4, 5], 2));
-    let key = AES128Key::new([0, 0, 0, 0]);
-    println!("{:#034x?}", key.generate_round_keys());
+    let key = AES128Key::new(
+        "This is a test12"
+            .to_string()
+            .into_bytes()
+            .chunks_exact(4)
+            .map(|c| {
+                c.into_iter()
+                    .enumerate()
+                    .map(|(i, &x)| (x as u32) << (3 - i) * 8)
+                    .sum()
+            })
+            .collect::<Vec<_>>()
+            .try_into()
+            .unwrap(),
+    );
+
+    let bytes: [u8; 16] = "I use Rust btw<3".as_bytes().try_into().unwrap();
+    let mut block = Block::from_bytes(bytes);
+
+    aesculap::encryption::encrypt_block(&mut block, key);
+
+    print!("{:#x?}", block);
+    // FIXME: This is not working properly
 }
