bef866850262c201a4a45b0f4ae7f3a3b94299c0
rabuu <rabuu.dev@gmail.com>
2023-04-29 00:07:40 +0200

    encryption: implement basic encryption algorithm

--- src/encryption.rs
+++ src/encryption.rs
@@ -2,4 +2,23 @@ use crate::{block::Block, key::AESKey};
 
 pub fn encrypt_block<const R: usize>(block: &mut Block, key: impl AESKey<R>) {
     let round_keys = key.round_keys();
+
+    for (i, round_key) in round_keys.into_iter().enumerate() {
+        if i == 0 {
+            block.add_round_key(round_key);
+            continue;
+        }
+
+        if i <= R - 2 {
+            block.sub_bytes();
+            block.shift_rows();
+            block.mix_columns();
+            block.add_round_key(round_key);
+            continue;
+        }
+
+        block.sub_bytes();
+        block.shift_rows();
+        block.add_round_key(round_key);
+    }
 }
