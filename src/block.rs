57b627ed2c2f8ce96c2264b80660c68af67c290c
rabuu <rabuu.dev@gmail.com>
2023-05-01 17:36:07 +0200

    block: add inverse functions (+ tests)

--- src/block.rs
+++ src/block.rs
@@ -55,10 +55,25 @@ impl Block {
         }
     }
 
+    pub fn sub_bytes_inv(&mut self) {
+        for col in &mut self.state {
+            *col = util::apply_sbox(*col, INVERSE_SBOX);
+        }
+    }
+
     pub fn shift_rows(&mut self) {
         let mut transposed = util::transpose_array2d(&self.state);
         for (i, row) in transposed.iter_mut().enumerate() {
             *row = util::rot_left(*row, i as isize);
+        }
+
+        self.state = util::transpose_array2d(&transposed);
+    }
+
+    pub fn shift_rows_inv(&mut self) {
+        let mut transposed = util::transpose_array2d(&self.state);
+        for (i, row) in transposed.iter_mut().enumerate() {
+            *row = util::rot_left(*row, -(i as isize));
         }
 
         self.state = util::transpose_array2d(&transposed);
@@ -77,6 +92,45 @@ impl Block {
                     1 => copy[0] ^ GMUL2[copy[1] as usize] ^ GMUL3[copy[2] as usize] ^ copy[3],
                     2 => copy[0] ^ copy[1] ^ GMUL2[copy[2] as usize] ^ GMUL3[copy[3] as usize],
                     3 => GMUL3[copy[0] as usize] ^ copy[1] ^ copy[2] ^ GMUL2[copy[3] as usize],
+                    _ => panic!(),
+                }
+            }
+        }
+    }
+
+    pub fn mix_columns_inv(&mut self) {
+        let copy = self.state;
+
+        for c in 0..4 {
+            let col = &mut self.state[c];
+            let copy = copy[c];
+
+            for r in 0..4 {
+                col[r] = match r {
+                    0 => {
+                        GMUL14[copy[0] as usize]
+                            ^ GMUL11[copy[1] as usize]
+                            ^ GMUL13[copy[2] as usize]
+                            ^ GMUL9[copy[3] as usize]
+                    }
+                    1 => {
+                        GMUL9[copy[0] as usize]
+                            ^ GMUL14[copy[1] as usize]
+                            ^ GMUL11[copy[2] as usize]
+                            ^ GMUL13[copy[3] as usize]
+                    }
+                    2 => {
+                        GMUL13[copy[0] as usize]
+                            ^ GMUL9[copy[1] as usize]
+                            ^ GMUL14[copy[2] as usize]
+                            ^ GMUL11[copy[3] as usize]
+                    }
+                    3 => {
+                        GMUL11[copy[0] as usize]
+                            ^ GMUL13[copy[1] as usize]
+                            ^ GMUL9[copy[2] as usize]
+                            ^ GMUL14[copy[3] as usize]
+                    }
                     _ => panic!(),
                 }
             }
@@ -121,6 +175,30 @@ mod tests {
     }
 
     #[test]
+    fn sub_bytes_inv_step() {
+        let state = [
+            [0x0, 0x1, 0x2, 0x3],
+            [0x4, 0x5, 0x6, 0x7],
+            [0x8, 0x9, 0xa, 0xb],
+            [0xc, 0xd, 0xe, 0xf],
+        ];
+
+        let sboxed_state = [
+            [0x63, 0x7c, 0x77, 0x7b],
+            [0xf2, 0x6b, 0x6f, 0xc5],
+            [0x30, 0x01, 0x67, 0x2b],
+            [0xfe, 0xd7, 0xab, 0x76],
+        ];
+
+        let mut block = Block::new(sboxed_state);
+        block.sub_bytes_inv();
+
+        let expected_block = Block::new(state);
+
+        assert_eq!(block, expected_block);
+    }
+
+    #[test]
     fn shift_rows_step() {
         let state = [
             [0x0, 0x1, 0x2, 0x3],
@@ -140,6 +218,30 @@ mod tests {
         block.shift_rows();
 
         let expected_block = Block::new(shifted_state);
+
+        assert_eq!(block, expected_block);
+    }
+
+    #[test]
+    fn shift_rows_inv_step() {
+        let state = [
+            [0x0, 0x1, 0x2, 0x3],
+            [0x4, 0x5, 0x6, 0x7],
+            [0x8, 0x9, 0xa, 0xb],
+            [0xc, 0xd, 0xe, 0xf],
+        ];
+
+        let shifted_state = [
+            [0x0, 0x5, 0xa, 0xf],
+            [0x4, 0x9, 0xe, 0x3],
+            [0x8, 0xd, 0x2, 0x7],
+            [0xc, 0x1, 0x6, 0xb],
+        ];
+
+        let mut block = Block::new(shifted_state);
+        block.shift_rows_inv();
+
+        let expected_block = Block::new(state);
 
         assert_eq!(block, expected_block);
     }
@@ -165,6 +267,31 @@ mod tests {
         block.mix_columns();
 
         let expected_block = Block::new(mixed_state);
+
+        assert_eq!(block, expected_block);
+    }
+
+    #[test]
+    fn mix_columns_inv_step() {
+        let state = [
+            [0xdb, 0x13, 0x53, 0x45],
+            [0xf2, 0x0a, 0x22, 0x5c],
+            [0xc6, 0xc6, 0xc6, 0xc6],
+            [0xd4, 0xd4, 0xd4, 0xd5],
+        ];
+
+        // see https://en.wikipedia.org/wiki/Rijndael_MixColumns#Test_vectors_for_MixColumn()
+        let mixed_state = [
+            [0x8e, 0x4d, 0xa1, 0xbc],
+            [0x9f, 0xdc, 0x58, 0x9d],
+            [0xc6, 0xc6, 0xc6, 0xc6],
+            [0xd5, 0xd5, 0xd7, 0xd6],
+        ];
+
+        let mut block = Block::new(mixed_state);
+        block.mix_columns_inv();
+
+        let expected_block = Block::new(state);
 
         assert_eq!(block, expected_block);
     }
