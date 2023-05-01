3b6f54fd7d20dca9c2cdb6488135428b8f2c72da
rabuu <rabuu.dev@gmail.com>
2023-05-01 23:41:41 +0200

    bin: write comments/abouts

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,17 @@ struct Cli {
 
 #[derive(Subcommand, Debug)]
 enum Command {
+    /// Encrypt data
     #[command(alias = "en")]
     Encrypt {
+        /// The key must have a size of 128, 192 or 256 bits (16, 24 or 32 bytes)
         #[arg(long, short)]
         key_file: PathBuf,
 
         #[command(flatten)]
         mode: Mode,
 
+        /// Padding is required to divide the data into even sized blocks
         #[arg(long, short)]
         #[arg(value_enum, default_value_t = Padding::Pkcs7)]
         padding: Padding,
@@ -34,8 +37,10 @@ enum Command {
         output: Output,
     },
 
+    /// Decrypt data
     #[command(alias = "de")]
     Decrypt {
+        /// The key must have a size of 128, 192 or 256 bits (16, 24 or 32 bytes)
         #[arg(long, short)]
         key_file: PathBuf,
 
@@ -46,6 +51,7 @@ enum Command {
         #[arg(value_enum, default_value_t = Padding::Pkcs7)]
         padding: Padding,
 
+        /// In CBC mode an IV with a size of 128 bits (16 bytes) is required
         #[arg(long)]
         #[arg(group = "iv")]
         iv_file: Option<PathBuf>,
@@ -61,31 +67,44 @@ enum Command {
 #[derive(Args, Debug)]
 #[group(required = true, multiple = false)]
 struct Mode {
+    /// Cipher Block Chaining mode
+    ///
+    /// An initialization vector (IV) is used and the blocks are chained together. It is generally more secure.
     #[arg(long)]
     #[arg(requires = "iv")]
     cbc: bool,
 
+    /// Electronic Code Book mode (not recommended)
+    ///
+    /// Each block is encrypted with the same key and algorithm. It is fast and easy but quite insecure.
     #[arg(long)]
     #[arg(conflicts_with = "iv")]
     ecb: bool,
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, ValueEnum, Debug)]
+enum Padding {
+    /// Padding is done according to PKCS #7 (recommended)
+    Pkcs7,
+
+    /// The blocks are filled with zeroes
+    Zero,
+
+    /// The data is not padded (may fail)
+    None,
 }
 
 #[derive(Args, Debug)]
 #[group(id = "iv")]
 #[group(multiple = false)]
 struct Iv {
+    /// In CBC mode an IV with a size of 128 bits (16 bytes) is required
     #[arg(long)]
     iv_file: Option<PathBuf>,
 
+    /// Generate a random IV and write it to a file
     #[arg(long)]
-    random_iv: bool,
-}
-
-#[derive(Clone, Copy, PartialEq, Eq, ValueEnum, Debug)]
-enum Padding {
-    Pkcs7,
-    Zero,
-    None,
+    random_iv: Option<PathBuf>,
 }
 
 #[derive(Args, Debug)]
