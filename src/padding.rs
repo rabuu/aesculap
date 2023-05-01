8db4690927dec0eda7efddbad2057c937c651e4d
rabuu <rabuu.dev@gmail.com>
2023-05-01 13:44:50 +0200

    padding: add BytePadding and ZeroPadding

--- src/padding.rs
+++ src/padding.rs
@@ -1,25 +1,32 @@
 #[derive(Debug)]
 pub enum Padding {
     Pkcs,
+    BytePadding(u8),
+    ZeroPadding,
 }
 
 impl Padding {
     pub fn pad<const B: usize>(&self, bytes: &[u8]) -> Vec<[u8; B]> {
         use Padding::*;
 
+        let mut blocks: Vec<[u8; B]> = bytes
+            .chunks_exact(B)
+            .map(|c| c.try_into().unwrap())
+            .collect();
+
+        let remainder = bytes.chunks_exact(B).remainder();
+
         match *self {
-            Pkcs => pkcs(bytes),
+            Pkcs => pkcs(&mut blocks, remainder),
+            BytePadding(byte) => pad_with_byte(&mut blocks, remainder, byte),
+            ZeroPadding => pad_with_byte(&mut blocks, remainder, 0),
         }
+
+        blocks
     }
 }
 
-fn pkcs<const B: usize>(bytes: &[u8]) -> Vec<[u8; B]> {
-    let mut blocks: Vec<[u8; B]> = bytes
-        .chunks_exact(B)
-        .map(|c| c.try_into().unwrap())
-        .collect();
-
-    let remainder = bytes.chunks_exact(B).remainder();
+fn pkcs<const B: usize>(blocks: &mut Vec<[u8; B]>, remainder: &[u8]) {
     let missing_bytes = B - remainder.len();
     let last_block: [u8; B] = remainder
         .iter()
@@ -30,6 +37,21 @@ fn pkcs<const B: usize>(bytes: &[u8]) -> Vec<[u8; B]> {
         .unwrap();
 
     blocks.push(last_block);
+}
 
-    blocks
+fn pad_with_byte<const B: usize>(blocks: &mut Vec<[u8; B]>, remainder: &[u8], byte: u8) {
+    if remainder.is_empty() {
+        return;
+    }
+
+    let missing_bytes = B - remainder.len();
+    let last_block: [u8; B] = remainder
+        .iter()
+        .chain(vec![byte; missing_bytes].iter())
+        .map(|&b| b)
+        .collect::<Vec<u8>>()
+        .try_into()
+        .unwrap();
+
+    blocks.push(last_block);
 }
