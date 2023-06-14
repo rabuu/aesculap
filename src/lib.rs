6cb11a5f9236f50b6e280da7b4db5b775d2bce33
rabuu <rabuu.dev@gmail.com>
2023-06-14 20:01:35 +0200

    docs: comment lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,17 @@ pub mod lookups;
 pub mod padding;
 pub mod util;
 
+/// AES encryption mode
+///
+/// Implemented modes:
+///
+/// - Electronic Code Book (ECB):
+///   Each block is encrypted with the same key and algorithm.
+///   It is fast and easy but quite insecure and therefore not recommended.
+///
+/// - Cipher Block Chaining (CBC):
+///   An initialization vector (IV) is used and the blocks are chained together.
+///   It is generally more secure.
 pub enum EncryptionMode {
     ECB,
     CBC(init_vec::InitializationVector),
