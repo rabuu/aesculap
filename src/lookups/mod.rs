7d6ee263882141852b55b64efb6ae84d8370b90f
rabuu <rabuu.dev@gmail.com>
2023-06-14 21:54:43 +0200

    docs: comment lookups module

--- src/lookups/mod.rs
+++ src/lookups/mod.rs
@@ -1,2 +1,6 @@
+//! Lookups module
+//!
+//! This module provides lookup tables for AES S-boxes and the Galois multiplication needed for the [mix columns step](crate::block::Block::mix_columns).
+
 pub mod gmul;
 pub mod sbox;
