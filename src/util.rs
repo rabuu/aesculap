4e1b6a1c29dfedb31ef81c2c05b0599cd4f4c3ee
rabuu <rabuu.dev@gmail.com>
2023-06-14 22:10:50 +0200

    docs: comment util.rs

--- src/util.rs
+++ src/util.rs
@@ -1,3 +1,8 @@
+//! Utilities module
+//!
+//! This module provides a few unrelated utility functions.
+
+/// Transpose a 2D array by exchanging rows and columns
 pub fn transpose_array2d<const N: usize>(inp: &[[u8; N]; N]) -> [[u8; N]; N] {
     let mut out = [[0; N]; N];
 
@@ -10,6 +15,7 @@ pub fn transpose_array2d<const N: usize>(inp: &[[u8; N]; N]) -> [[u8; N]; N] {
     out
 }
 
+/// Cyclically rotate a byte array by a given offset
 pub fn rot_left<const N: usize>(mut bytes: [u8; N], shift: isize) -> [u8; N] {
     if shift == 0 {
         return bytes;
@@ -24,6 +30,7 @@ pub fn rot_left<const N: usize>(mut bytes: [u8; N], shift: isize) -> [u8; N] {
     bytes
 }
 
+/// Substitute each byte of an array using a given S-box
 pub fn apply_sbox<const N: usize>(mut bytes: [u8; N], sbox: [u8; 256]) -> [u8; N] {
     for byte in &mut bytes {
         *byte = sbox[*byte as usize];
@@ -32,6 +39,7 @@ pub fn apply_sbox<const N: usize>(mut bytes: [u8; N], sbox: [u8; 256]) -> [u8; N
     bytes
 }
 
+/// Interprete four bytes as an `u32`
 pub fn bytes_as_u32(bytes: [u8; 4]) -> u32 {
     (bytes[3] as u32)
         | ((bytes[2] as u32) << 8)
