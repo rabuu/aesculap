179719c49ca1e026980b49da37d6d2c1b45f0f38
rabuu <rabuu.dev@gmail.com>
2023-06-14 21:24:21 +0200

    docs: comment iv.rs

--- src/init_vec.rs
+++ src/init_vec.rs
@@ -1,18 +1,30 @@
+//! Initialization vector module
+//!
+//! This module provides a wrapper type for a [Block] that is used as initialization vector (IV).
+
 use crate::block::Block;
 
+/// Initialization vector (IV) wrapper
+///
+/// This type wraps a [Block] that is used as IV.
+///
+/// For reference, see the [Wikipedia article](https://en.wikipedia.org/wiki/Initialization_vector)
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub struct InitializationVector(Block);
 
 impl InitializationVector {
+    /// Constructor that takes 16 bytes
     pub fn from_bytes(bytes: [u8; 16]) -> Self {
         Self(Block::from_bytes(bytes))
     }
 
+    /// Construct a random IV
     #[cfg(feature = "rand")]
     pub fn random() -> Self {
         Self(Block::from_bytes(rand::random()))
     }
 
+    /// Get the inner bytes
     pub fn into_bytes(&self) -> [u8; 16] {
         self.0.dump_bytes()
     }
